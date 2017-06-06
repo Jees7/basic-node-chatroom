1d76a6966a61f34514bb41d83dce7e4cd337667a
Jeesang Kim <arsenalkim@gmail.com>
2017-06-06 18:47:27 +0900

    Fix errors

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,46 +1,43 @@
 var Chat = function(socket) {
-    this.socket = socket;
-}
+  this.socket = socket;
+};
 
-/* 채팅 메시지를 전송 */
 Chat.prototype.sendMessage = function(room, text) {
-    var message = {
-        room: room,
-        text: text
-    };
-    this.socket.emit('message', message);
+  var message = {
+    room: room,
+    text: text
+  };
+  this.socket.emit('message', message);
 };
 
-/* 채팅방 변경 */
 Chat.prototype.changeRoom = function(room) {
-    this.socket.emit('join', {
-        newRoom: room
-    });
+  this.socket.emit('join', {
+    newRoom: room
+  });
 };
 
-/* 채팅 명령 처리 */
 Chat.prototype.processCommand = function(command) {
-    var words = command.split(' ');
-    var command = words[0].substring(1, words[0].length).toLowerCase();
-    var message = false;
+  var words = command.split(' ');
+  var command = words[0]
+                .substring(1, words[0].length)
+                .toLowerCase();
+  var message = false;
 
-    switch(command) {
-        case 'join':        
-        words.shift();
-        var room = words.join(' ');
-        this.changeRoom(room);
-        break;
+  switch(command) {
+    case 'join':
+      words.shift();
+      var room = words.join(' ');
+      this.changeRoom(room);
+      break;
+    case 'nick':
+      words.shift();
+      var name = words.join(' ');
+      this.socket.emit('nameAttempt', name);
+      break;
+    default:
+      message = 'Unrecognized command.';
+      break;
+  };
 
-        case 'nick':
-        words.shift();
-        var name = words.join(' ');
-        this.socket.emit('nameAttempt', name);
-        break;
-
-        default:
-        message = 'Unrecognized command.';
-        break;
-    }
-    return message;
+  return message;
 };
-
