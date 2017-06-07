838203643b8784ed39bcd6d1da3fd31ce2472447
Jeesang Kim <arsenalkim@gmail.com>
2017-06-07 23:05:47 +0900

    Update README

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # basic-node-chatroom
 Personal practice node.js programming for chatroom
+
+## Functions
+* 손님 닉네임 부여
+* 채팅방 변경 요청
+* 닉네임 변경 요청
+* 채팅 메시지 전송
+* 채팅방 생성
+* 사용자 접속 해제
--- public/javascripts/chat_ui.js
+++ public/javascripts/chat_ui.js
@@ -28,10 +28,10 @@ function processUserInput(chatApp, socket) {
 /* Client-side, 애플리케이션 초기화 */
 var socket = io.connect();
 
-$(document).ready(function() {
+$(document).ready(function () {
   var chatApp = new Chat(socket);
 
-  socket.on('nameResult', function(result) {
+  socket.on('nameResult', function (result) {
     var message;
 
     if (result.success) {
@@ -42,7 +42,7 @@ $(document).ready(function() {
     $('#messages').append(divSystemContentElement(message));
   });
 
-  socket.on('joinResult', function(result) {
+  socket.on('joinResult', function (result) {
     $('#room').text(result.room);
     $('#messages').append(divSystemContentElement('Room changed.'));
   });
@@ -52,29 +52,29 @@ $(document).ready(function() {
     $('#messages').append(newElement);
   });
 
-  socket.on('rooms', function(rooms) {
+  socket.on('rooms', function (rooms) {
     $('#room-list').empty();
 
-    for(var room in rooms) {
+    for (var room in rooms) {
       room = room.substring(1, room.length);
       if (room != '') {
         $('#room-list').append(divEscapedContentElement(room));
       }
     }
 
-    $('#room-list div').click(function() {
+    $('#room-list div').click(function () {
       chatApp.processCommand('/join ' + $(this).text());
       $('#send-message').focus();
     });
   });
 
-  setInterval(function() {
+  setInterval(function () {
     socket.emit('rooms');
   }, 1000);
 
   $('#send-message').focus();
 
-  $('#send-form').submit(function() {
+  $('#send-form').submit(function () {
     processUserInput(chatApp, socket);
     return false;
   });
